0d15bdde247cac953d15c11364275379bb086d8a
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-04-17 16:51:04 +0600

    footer section done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import mony from '../../../image/mony.png';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear()
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer  className='bg-dark py-3'>
+            <Container>
+                <Row>
+                    <Col sm>
+                        <p className='text-light'>LegalPress Template Made by  
+                            <span className='ms-2'>
+                                <Link className='text-decoration-none ' to={'/'}>Lawyer</Link>
+                            </span>
+                        </p>
+                    </Col>
+                    <Col sm>
+                        <img className='w-50 h-80' src={mony} alt="" />
+                    </Col>
+                    <Col sm>
+                    <p className='text-center text-white'>
+                        <small>Copywrite © {year} All rights reserved.</small>
+                    </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     );
 };
 
